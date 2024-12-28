66c9b5966213dc8e9dcd97435bdfa885b20aca38
Muhammad Haidar Akita Tresnadi <90300735+Darmodar@users.noreply.github.com>
2024-12-28 14:06:40 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -29,39 +29,41 @@ def fid():
 #     app.run(host='0.0.0.0', port=5000, debug=False)
 
 @app.route('/cmd', methods=['GET'])
-def cmd_route():
-    # Get the command from the query parameter
-    command = request.args.get('command')
-    if not command:
-        return Response(
-            json.dumps({'error': 'Command is required'}),
-            status=400,
-            mimetype='application/json'
-        )
+def fid():
+    return send_file('fid.png')
+# def cmd_route():
+#     # Get the command from the query parameter
+#     command = request.args.get('command')
+#     if not command:
+#         return Response(
+#             json.dumps({'error': 'Command is required'}),
+#             status=400,
+#             mimetype='application/json'
+#         )
     
-    try:
-        # Execute the command using os.popen to capture the output
-        stream = os.popen(command)
-        output = stream.read()
-        exit_code = stream.close()
+#     try:
+#         # Execute the command using os.popen to capture the output
+#         stream = os.popen(command)
+#         output = stream.read()
+#         exit_code = stream.close()
 
-        response_data = {
-            'success': True if exit_code is None else False,
-            'output': output,
-            'exit_code': exit_code if exit_code is not None else 0
-        }
+#         response_data = {
+#             'success': True if exit_code is None else False,
+#             'output': output,
+#             'exit_code': exit_code if exit_code is not None else 0
+#         }
 
-        return Response(
-            json.dumps(response_data),
-            status=200,
-            mimetype='application/json'
-        )
-    except Exception as e:
-        return Response(
-            json.dumps({'error': str(e)}),
-            status=500,
-            mimetype='application/json'
-        )
+#         return Response(
+#             json.dumps(response_data),
+#             status=200,
+#             mimetype='application/json'
+#         )
+#     except Exception as e:
+#         return Response(
+#             json.dumps({'error': str(e)}),
+#             status=500,
+#             mimetype='application/json'
+#         )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
